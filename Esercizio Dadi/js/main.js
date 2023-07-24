037b5472f042ec2bb934170c24cee60935a2decc
robertoraciti <rob.raciti97@gmail.com>
2023-07-24 14:50:31 +0200

    Custom ex dadi

--- Esercizio Dadi/js/main.js
+++ Esercizio Dadi/js/main.js
@@ -1,11 +1,18 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+const rollDice = document.getElementById("roll-dice");
 
-const userNumber = Math.round(Math.random() * 5) + 1;
-const computerNumber = Math.round(Math.random() * 5) + 1;
-
-if (userNumber > computerNumber) {
-  alert("L'utente ha vinto");
-} else if (userNumber < computerNumber) {
-  alert("Il computer ha vinto");
-} else alert("Parità");
+rollDice.addEventListener("click", function () {
+  const userNumber = Math.round(Math.random() * 5) + 1;
+  const computerNumber = Math.round(Math.random() * 5) + 1;
+  const userResult = document.getElementById("user-result");
+  const computerResult = document.getElementById("computer-result");
+  userResult.innerHTML = "Risultato utente: " + userNumber;
+  computerResult.innerHTML = "Risultato computer: " + computerNumber;
+  const result = document.getElementById("result");
+  if (userNumber > computerNumber) {
+    result.innerHTML = "L'utente ha vinto";
+  } else if (userNumber < computerNumber) {
+    result.innerHTML = "Il computer ha vinto";
+  } else result.innerHTML = "Parità";
+});
